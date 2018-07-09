561802d2a07d88e76f7c0049b40a78c429dfe36a
Luping Lin <lupinglin@gmail.com>
2018-07-09 01:21:46 -0700

    set FAB onclicklistener so pressedTranslationZ can take effect.

--- app/src/main/java/com/luping/photos/view/PhotoFragment.java
+++ app/src/main/java/com/luping/photos/view/PhotoFragment.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -66,6 +67,11 @@ public class PhotoFragment extends Fragment {
         if (activity.getSupportActionBar() != null) {
             activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        FloatingActionButton fab = activity.findViewById(R.id.fav_fab);
+        fab.setOnClickListener(view -> {
+            Log.d(TAG, "FAB clicked");
+        });
     }
 
     @Override
